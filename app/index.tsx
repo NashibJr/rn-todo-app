94b75a09df7aadf8f30bd111ced2708b134686da
NashibjrII <nashib@tolamedia.ug>
2024-09-15 03:32:55 -0700

    Feat: Added the welcome screen

---
+++ app/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Href, useRouter } from "expo-router";
+
+const WelcomeScreen = () => {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const navigate = setInterval(() => {
+      router.replace("/auth/login" as unknown as Href<string | object>);
+    }, 3000);
+
+    return () => clearInterval(navigate);
+  }, []);
+
+  return (
+    <View style={[styles.container]}>
+      <Text style={[styles.text]}>todo app</Text>
+    </View>
+  );
+};
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: "auto",
+  },
+  text: {
+    fontSize: 36,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    color: "#FF4500",
+  },
+});
