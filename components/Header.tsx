91d9421e1acba0dce8c85689797b906d6b5f33ca
NashibjrII <nashib@tolamedia.ug>
2024-09-15 10:54:02 -0700

    Feat: Added the header section

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,28 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Feather from "@expo/vector-icons/Feather";
 
 const Header = () => {
   return (
-    <View>
-      <Text>Header</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>todo app</Text>
+      <Feather name="menu" size={35} color="black" />
     </View>
   );
 };
 
 export default Header;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    color: "#ff4500",
+  },
+});
